59ebc82ad633bef735b370c5a21f7ca3b7324c8e
Od1e <stopsniffingaxe@gmail.com>
2022-05-07 10:08:13 -0400

    add basic main page

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,13 @@
 <template>
-  <Tutorial />
+  <div class="container w-screen h-screen flex justify-center items-center flex-col">
+    <h1 class="text-lg font-semibold">Last.FM Friends</h1>
+    <div class="bg-gray-500 text-white p-4 rounded-lg">
+      <div class="flex flex-col items-center">
+        Enter your username to view your friends
+        <input type="text" class="rounded-full w-40 mt-3 transition-all duration-300 ease-in-out focus:p-2 focus:w-60 focus:text-2xl text-gray-600 text-center">
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
